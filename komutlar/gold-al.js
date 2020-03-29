4b8868c0c87171023d9ddd67a863f739e7f2a596
Glitch (davet-yoneticisi) <none>
2020-03-29 18:34:16 +0000

    🎆😻 Checkpoint ./komutlar/gold-ver.js:3266250/1182 ./komutlar/gold-al.js:3266250/1183

--- komutlar/gold-al.js
+++ komutlar/gold-al.js
@@ -2,15 +2,13 @@ const db = require('quick.db')
 const Discord = require('discord.js')
 const client = new Discord.Client();
 exports.run = async (bot, message, args) => {
- //
-
-  if (message.author.id !== '533292083172081695') return;
+ 
   let nesne = args[0]
   if (!nesne) return message.channel.send('Bir kullanıcının IDsini girmelisin?')
   
-  db.delete(`tios_gold${nesne}`, 'gold')
+  db.delete(`gold_${nesne}`)
   
-  message.channel.send(`**${nesne}** IDli Kullanıcı Artık Gold Üye Değil`)
+  message.channel.send(`**${nesne}** IDli kullanıcı artık gold üye değil!`)
 }
 exports.conf = {
   enabled: true,
@@ -19,7 +17,7 @@ exports.conf = {
   permLevel: 4
 };
 exports.help = {
-  name: 'gold-al',
+  name: 'goldçıkar',
   description: '[Admin Komutu]',
   usage: 'karaliste <ID>'
 };
--- komutlar/gold-ver.js
+++ komutlar/gold-ver.js
@@ -2,13 +2,11 @@ const db = require('quick.db')
 const Discord = require('discord.js')
 const client = new Discord.Client();
 exports.run = async (bot, message, args) => {
- //
-
-  if (message.author.id !== '533292083172081695') return;
+ 
   let nesne = args[0]
   if (!nesne) return message.channel.send('Bir kullanıcının IDsini girmelisin?')
   
-  db.set(`tios_gold${nesne}`, 'gold')
+  db.set(`gold_${nesne}`, 'gold')
   
   message.channel.send(`**${nesne}** IDli kullanıcı artık gold üye oldu!`)
 }
@@ -19,7 +17,7 @@ exports.conf = {
   permLevel: 4
 };
 exports.help = {
-  name: 'gold-ver',
+  name: 'goldyap',
   description: '[Admin Komutu]',
   usage: 'karaliste <ID>'
 };
