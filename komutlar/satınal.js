de553ebd8bdd13611e209b4827187ac4c3c88d41
Glitch (davet-yoneticisi) <none>
2020-03-29 07:44:07 +0000

    🐞⛵️ Checkpoint ./komutlar/satınal.js:3266250/5642

---
+++ komutlar/satınal.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+let botid = ('602024501710159882') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`${client.user.username} `, client.user.avatarURL)
+        .setColor('0x36393E')
+        .setTitle(`${client.user.username} - Disko Yardım Menüsü`)
+        .setDescription(` | • Merhabalar !\n\n  | • Sizde Discord Botu / Discord Sunucusu / Discord Üye SAtın Almak Mı İstiyorsunuz ?\n\n  | • \n`)  
+        .setThumbnail(client.user.avatarURL)
+        .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=677145750043361320&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/rC7nHHp) **|** [Web Sitesi |YAKINDA|](https://discord.gg/rC7nHHp)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
+    return message.channel.sendEmbed(embed);
+  
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['satınal'],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'diskoyardım',
+  description: '',
+  usage: ''
+};
