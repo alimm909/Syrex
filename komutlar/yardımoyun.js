b196ddd38d3f1a4947c3b2a31040542ed8d5b174
Glitch (davet-yoneticisi) <none>
2020-04-07 21:22:30 +0000

    🦀🎑 Checkpoint ./komutlar/yardımoyun.js:3266250/507 ./komutlar/oyunmayıntarlası.js:3266250/955 ./package.json:3266250/2660

--- komutlar/yardımoyun.js
+++ komutlar/yardımoyun.js
@@ -9,7 +9,7 @@ exports.run = (client, message, args) => {
         .setAuthor(`${client.user.username} `, client.user.avatarURL)
         .setColor('BLUE')
         .setTitle(`${client.user.username} - Oyun Listesi`)
-        .setDescription(` • | **${ayarlar.prefix}slot** : Gerçek Hayattaki Slot Oyununu Oynarsınız.\n• | **${ayarlar.prefix}kelime-tahmini** : Bot İle Kelime Tahmin Yarışması Oynarsınız  \`( #BETA )\`.\n • | **${ayarlar.prefix}adamasmaca** : Bot İle Adam Asmaca Oynarsınız. \`( #BETA )\`\n • | **${ayarlar.prefix}ototagsistemi** : Otomatik Tag Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}sa-as** : Otomatik Selamlaşmayı Açıp Kapatırsınız.\n • | **${ayarlar.prefix}hg-bb** : Özelden Gönderilen Otomatik Karşılama ve Uğurlama Komutunu Açıp Kapatırsınız.`)  
+        .setDescription(`• | **${ayarlar.prefix}kelime-tahmini** : Bot İle Kelime Tahmin Yarışması Oynarsınız  \`( #BETA )\`.\n • | **${ayarlar.prefix}adamasmaca** : Bot İle Adam Asmaca Oynarsınız. \`( #BETA )\``)  
         .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=677145750043361320&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/YETK66G) **|** [Web Sitemiz](https://linlordscode.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
         .setFooter(`SyreX | Bot Sürümü : v0.2 | Varsayılan Prefix : !`, client.user.avatarURL)
     return message.channel.sendEmbed(embed);
@@ -20,12 +20,12 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['sistem'],
+  aliases: ['oyunlar'],
   permLevel: 0,
 };
 
 exports.help = {
-  name: 'sistem',
+  name: 'oyun',
   description: '',
   usage: ''
 };
