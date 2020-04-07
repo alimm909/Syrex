fa7f92f4f22e8db5c03f150334fede5731a08ab5
Glitch (davet-yoneticisi) <none>
2020-04-07 19:51:57 +0000

    👮👣 Checkpoint ./komutlar/yardımsistemotorol.js:3266250/376 ./komutlar/yardımsistemsayac.js:3266250/1775

--- komutlar/yardımsistemsayac.js
+++ komutlar/yardımsistemsayac.js
@@ -8,8 +8,8 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         .setAuthor(`${client.user.username} `, client.user.avatarURL)
         .setColor('BLUE')
-        .setTitle(`${client.user.username} - Otorol Komutları Listesi`)
-        .setDescription(` • | **${ayarlar.prefix}otorol** : Sunucuya Yeni Katılan Kullanıcılara Verilecek Rolü Ayarlarsınız.\n • | **${ayarlar.prefix}otorol-sıfırla** : Ayarlanmış Otorol Sistemini Sıfırlarsınız.\n • | **${ayarlar.prefix}otorol-mesaj** : Otorol Uygulandığında Gönderilecek Mesajı Oluşturursunuz.\n • | **${ayarlar.prefix}otorol-mesaj-sıfırla** : Otorol Mesajını Sıfırlarsınız.`)  
+        .setTitle(`${client.user.username} - Sayaç Komutları Listesi`)
+        .setDescription(` • | **${ayarlar.prefix}sayaç** : Belirlediğiniz Hedefe Ulaşma Yolunda Üyelerinizi Sayar.\n • | **${ayarlar.prefix}sayaç-sıfırla** : Ayarlanmış Sayaç Sistemini Sıfırlarsınız.\n • | **${ayarlar.prefix}otorol-mesaj** : Otorol Uygulandığında Gönderilecek Mesajı Oluşturursunuz.\n • | **${ayarlar.prefix}otorol-mesaj-sıfırla** : Otorol Mesajını Sıfırlarsınız.`)  
         .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=677145750043361320&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/YETK66G) **|** [Web Sitemiz](https://linlordscode.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
         .setFooter(`SyreX | Bot Sürümü : v0.2 | Varsayılan Prefix : !`, client.user.avatarURL)
     return message.channel.sendEmbed(embed);
