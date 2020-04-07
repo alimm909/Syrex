880ea9baf8614a801e0c05d572b597fa278718b9
Glitch (davet-yoneticisi) <none>
2020-04-07 21:10:45 +0000

    🚝🍗 Checkpoint ./komutlar/yardım.js:3266250/60 ./komutlar/oyunkelimetahmini.js:3266250/3939 ./komutlar/oyunkelimetahminipuan.js:3266250/843 ./komutlar/yardımoyun.js:3266250/1998 ./komutlar/oyunadamasmaca.js:3266250/3387 ./kelimeler.json:3266250/194

--- komutlar/yardımoyun.js
+++ komutlar/yardımoyun.js
@@ -8,9 +8,8 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         .setAuthor(`${client.user.username} `, client.user.avatarURL)
         .setColor('BLUE')
-        .setTitle(`${client.user.username} - Yardım Menüsü`)
-        .setThumbnail(client.user.avatarURL)
-        .setDescription(` • | **${ayarlar.prefix}davetsistemi** : Davet Sistemi Hakkında Bütün Bilgileri Listelersiniz.\n • | **${ayarlar.prefix}moderasyon** : Yetkililerin Kullanabileceği Komutları Listelersiniz.\n • | **${ayarlar.prefix}sistem** : Kullanabileceğiniz Gelişmiş Sistem Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}bot** : Bot Hakkında Ve Diğer Kategori Komutlarını Görürsünüz.\n • | **${ayarlar.prefix}premium** : Premium Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}oyunlar** : \`( YENİ )\`Discord Üzerinden Oynanabilen Basit Oyunları Listelersiniz.\n • | **${ayarlar.prefix}kayıtsistemi** : \`( YENİ )\`Ayarlanabilir Gelişmiş Kayıt Sisteminin Komutlarını Listelersiniz.`)  
+        .setTitle(`${client.user.username} - Oyun Listesi`)
+        .setDescription(` • | **${ayarlar.prefix}slot** : Gerçek Hayattaki Slot Oyununu Oynarsınız.\n• | **${ayarlar.prefix}kelime-tahmini** : Bot İle Kelime Tahmin Yarışması Oynarsınız  \`( #BETA )\`.\n • | **${ayarlar.prefix}adamasmaca** : Bot İle Adam Asmaca Oynarsınız. \`( #BETA )\`\n • | **${ayarlar.prefix}ototagsistemi** : Otomatik Tag Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}sa-as** : Otomatik Selamlaşmayı Açıp Kapatırsınız.\n • | **${ayarlar.prefix}hg-bb** : Özelden Gönderilen Otomatik Karşılama ve Uğurlama Komutunu Açıp Kapatırsınız.`)  
         .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=677145750043361320&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/YETK66G) **|** [Web Sitemiz](https://linlordscode.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
         .setFooter(`SyreX | Bot Sürümü : v0.2 | Varsayılan Prefix : !`, client.user.avatarURL)
     return message.channel.sendEmbed(embed);
@@ -21,12 +20,12 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['yardım','y','help','h'],
+  aliases: ['sistem'],
   permLevel: 0,
 };
 
 exports.help = {
-  name: 'yardım',
+  name: 'sistem',
   description: '',
   usage: ''
 };
