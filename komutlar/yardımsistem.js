62ee1506a00466ef265ccede8d65621419f1b36f
Glitch (davet-yoneticisi) <none>
2020-04-07 18:16:46 +0000

    🎄👡 Checkpoint ./komutlar/yardımsistem.js:3266250/547

--- komutlar/yardımsistem.js
+++ komutlar/yardımsistem.js
@@ -9,7 +9,7 @@ exports.run = (client, message, args) => {
         .setAuthor(`${client.user.username} `, client.user.avatarURL)
         .setColor('BLUE')
         .setTitle(`${client.user.username} - Sistem Komutları Listesi`)
-        .setDescription(` • | **${ayarlar.prefix}otorolsistemi** : Otorol Komut.\n • | **!moderasyon** : Yetkililerin Kullanabileceği Komutları Listelersiniz.\n • | **${ayarlar.prefix}sistem** : Sistem Komutarını Listelersiniz.\n • | **${ayarlar.prefix}bot** : Bot Hakkındaki Bilgileri Ve Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}premium** : Premium Sistemi Komutlarını Listelersiniz.`)  
+        .setDescription(` • | **${ayarlar.prefix}korumasistemi** : Bütün Koruma Komutlarını Listelersiniz.\n• | **${ayarlar.prefix}otorolsistemi** : Otorol Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}sayaçsistemi** : Sayaç Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}ototagsistemi** : Otomatik Tag Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}bot** : Bot Hakkındaki Bilgileri Ve Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}premium** : Premium Sistemi Komutlarını Listelersiniz.`)  
         .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=677145750043361320&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/YETK66G) **|** [Web Sitemiz](https://linlordscode.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
         .setFooter(`SyreX | Bot Sürümü : v0.2 | Varsayılan Prefix : !`, client.user.avatarURL)
     return message.channel.sendEmbed(embed);
