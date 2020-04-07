a1d0e63a7a00d89c42d48ce92e05508875c16b97
Glitch (davet-yoneticisi) <none>
2020-04-07 17:53:57 +0000

    ⛑⛵️ Checkpoint ./komutlar/yardımdavetsistemi.js:3266250/155 ./komutlar/yardımmoderasyon.js:3266250/3130

---
+++ komutlar/yardımmoderasyon.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+let botid = ('602024501710159882') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`${client.user.username} `, client.user.avatarURL)
+        .setColor('BLUE')
+        .setTitle(`${client.user.username} - Moderasyon Komut Listesi`)
+        .setDescription(` • | **${ayarlar.prefix}ban** : Etiketlediğiniz Kullanıcıyı Sunucudan Yasaklarsınız.\n • | **${ayarlar.prefix}kick** : Etiketlediğiniz Kullanıcıyı Sunucudan Atarsınız.\n • | **${ayarlar.prefix}unban** : İdsini Girdiğiniz Kullanıcının Sunucudaki Yasaklamasını Kaldırırsınız.\n • | **${ayarlar.prefix}kanal-kilit** : Komutu Yazdığınız Kanalı Belirttiğiniz Süre Boyunca Mesajlaşmaya Kapatır.\n • | **${ayarlar.prefix}mute** : Etiketlediğiniz Kullanıcıyı Belirtiğiniz Süre Kadar Mutelersiniz.\n • | **${ayarlar.prefix}takma-ad** : Etiketlediğiniz Kullanıcının Kullanıcı Adını Ayarlarsınız.\n • | **${ayarlar.prefix}rol-al** : Etiketlediğiniz Kullanıcıya Belirttiğiniz Rolü Verirsiniz.\n • | **${ayarlar.prefix}rol-al** : Etiketlediğiniz Kullanıcıdan Belirttiğiniz Rolü Alırsınız.\n • | **${ayarlar.prefix}reklam-taraması** : Özel Durumunda ve `)  
+        .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=677145750043361320&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/YETK66G) **|** [Web Sitemiz](https://linlordscode.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+        .setFooter(`SyreX | Bot Sürümü : v0.2 | Varsayılan Prefix : !`, client.user.avatarURL)
+    return message.channel.sendEmbed(embed);
+  
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['mod','moderasyon','yetkili'],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'moderasyon',
+  description: '',
+  usage: ''
+};
