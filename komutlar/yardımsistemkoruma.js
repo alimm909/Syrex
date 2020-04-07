80dc45d8183d5ae79ab55b0b310d2860739bb15d
Glitch (davet-yoneticisi) <none>
2020-04-07 18:39:50 +0000

    🐥🌻 Checkpoint ./komutlar/yardımsistem.js:3266250/423 ./komutlar/yardımsistemkoruma.js:3266250/2515

--- komutlar/yardımsistemkoruma.js
+++ komutlar/yardımsistemkoruma.js
@@ -8,8 +8,8 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         .setAuthor(`${client.user.username} `, client.user.avatarURL)
         .setColor('BLUE')
-        .setTitle(`${client.user.username} - Sistem Komutları Listesi`)
-        .setDescription(` • | **${ayarlar.prefix}korumasistemi** : Bütün Koruma Komutlarını Listelersiniz.\n• | **${ayarlar.prefix}otorolsistemi** : Otorol Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}sayaçsistemi** : Sayaç Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}ototagsistemi** : Otomatik Tag Komutlarını Listelersiniz.\n • | **${ayarlar.prefix}sa-as** : Otomatik Selamlaşmayı Açıp Kapatırsınız.\n • | **${ayarlar.prefix}hg-bb** : Özelden Gönderilen Otomatik Karşılama ve Uğurlama Komutunu Açıp Kapatırsınız.`)  
+        .setTitle(`${client.user.username} - Koruma Komutları Listesi`)
+        .setDescription(` • | **${ayarlar.prefix}rol-koruma** : Rol Koruma Sistemini Açarsınız.\n • | **${ayarlar.prefix}rol-koruma-sıfırla** : Rol Koruma Sistemini Sıfırlarsınız.\n • | **${ayarlar.prefix}kanal-koruma** : Kanal Koruma Sistemini Açarsınız.\n • | **${ayarlar.prefix}kanal-koruma-sıfırla** : Kanal Koruma Sistemini Sıfırlarsınız.\n • | **${ayarlar.prefix}reklam-engel** : Sunucunuzda Yapılan Reklamları Önlemek İçin Kurulan Sistemi Açıp Kapatırsınız.\n • | **${ayarlar.prefix}ever-engel** : Everyone Kullanımlarını Tamamı İle Engeller.`)  
         .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=677145750043361320&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/YETK66G) **|** [Web Sitemiz](https://linlordscode.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
         .setFooter(`SyreX | Bot Sürümü : v0.2 | Varsayılan Prefix : !`, client.user.avatarURL)
     return message.channel.sendEmbed(embed);
@@ -20,12 +20,12 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['sistem'],
+  aliases: ['koruma','korumasistemi','koruma-sistemi'],
   permLevel: 0,
 };
 
 exports.help = {
-  name: 'sistem',
+  name: 'koruma',
   description: '',
   usage: ''
 };
